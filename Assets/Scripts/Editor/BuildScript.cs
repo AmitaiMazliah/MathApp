e1c1b12d8f6ad612e7c9043e694db0ad05e8d35d
amitai <amitaimazliah@gmail.com>
2023-08-20 17:27:38 +0300

    Fix UI scale

--- Assets/Scripts/Editor/BuildScript.cs
+++ Assets/Scripts/Editor/BuildScript.cs
@@ -5,9 +5,30 @@ using UnityEditor.AddressableAssets.Settings;
 public static class BuildScript
 {
     const string initScene = "Assets/Scenes/Initialization.unity";
+    
+    [MenuItem("Build/Windows")]
+    public static void BuildWindows()
+    {
+        PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
+        var buildPlayerOptions = new BuildPlayerOptions
+        {
+            scenes = new[] { initScene },
+            locationPathName = "Builds/Windows/MathApp.exe",
+            target = BuildTarget.StandaloneWindows64,
+            options = BuildOptions.CompressWithLz4HC,
+            subtarget = (int)StandaloneBuildSubtarget.Player
+        };
+
+        AddressableAssetSettings.CleanPlayerContent();
+        AddressableAssetSettings.BuildPlayerContent();
+
+        Console.WriteLine("Building...");
+        BuildPipeline.BuildPlayer(buildPlayerOptions);
+        Console.WriteLine("Built.");
+    }
 
     [MenuItem("Build/Web")]
-    public static void BuildWindowsServer()
+    public static void BuildWeb()
     {
         PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
         var buildPlayerOptions = new BuildPlayerOptions
